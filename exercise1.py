dee68472444f155e42c87200834e6f9aff8ea380
Antonio Martos Rodríguez <antonio.martos@badgermapping.com>
2021-11-26 14:38:56 +0100

    Adding calculate fuel need from mass

--- exercise1.py
+++ exercise1.py
@@ -1,15 +1,13 @@
 from typing import NamedTuple
-import math
 
 def calculate_fuel_list_from_mass_list(mass_list):
     pass
 
 def calculate_fuel_needed_from_mass(mass):
-    fuel_needed = (math.trunc(mass/3.0)) - 2
-    return fuel_needed
+    return 0 if mass < 6 else mass // 3.0 - 2
 
 class Module(NamedTuple):
-    mass: int
+    mass: float
 
 # if __name__ == '__main__':
 #     print_hi('PyCharm')
--- test/test.py
+++ test/test.py
@@ -12,11 +12,43 @@ class TestExercise1(unittest.TestCase):
 
         self.assertEqual(fuel_needed, expected_fuel_needed)
 
-    def test_calculate_fuel_list_from_mass_list(self):
-        mass_list = [12, 14, 1969, 100756]
-        expected_fuel_list = [2, 2, 654, 33583]
+    def test_when_mass_lower_than_6_then_the_fuel_calculated_result_is_zero(self):
+        module = Module(mass=1)
+        expected_fuel_needed = 0
 
-        fuel_list = calculate_fuel_list_from_mass_list(mass_list)
-        self.assertEqual(expected_fuel_list, fuel_list)
+        fuel_needed = calculate_fuel_needed_from_mass(module.mass)
 
+        self.assertEqual(expected_fuel_needed, fuel_needed)
 
+    def test_when_mass_is_negative_then_the_fuel_calculated_result_is_zero(self):
+        module = Module(mass=-1)
+        expected_fuel_needed = 0
+
+        fuel_needed = calculate_fuel_needed_from_mass(module.mass)
+
+        self.assertEqual(expected_fuel_needed, fuel_needed)
+
+    def test_when_mass_has_decimals_then_the_fuel_calculated_result_is_zero(self):
+        module = Module(mass=7.5)
+        expected_fuel_needed = 0
+
+        fuel_needed = calculate_fuel_needed_from_mass(module.mass)
+
+        self.assertEqual(expected_fuel_needed, fuel_needed)
+
+    def test_when_mass_has_zero_then_the_fuel_calculated_result_is_zero(self):
+        module = Module(mass=0)
+        expected_fuel_needed = 0
+
+        fuel_needed = calculate_fuel_needed_from_mass(module.mass)
+
+        self.assertEqual(expected_fuel_needed, fuel_needed)
+
+    def test_given_a_modules_list_when_calculate_fuel_list_then_a_fuel_amount_list_is_returned(self):
+        modules = [Module(12)]
+        expected_fuels = [2]
+
+        exercise1 = exercise1
+        #Research how to Mock the method called via "calculate_fuels_from_modules"
+        fuels = calculate_fuels_from_modules(modules)
+        self.assertEqual(expected_fuels, fuels)
